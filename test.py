0c478e5ad27833ce695501b26701b4e832a1d788
HimaHina <blankov.boris@mail.ru>
2022-07-26 19:32:26 +0800

    1

---
+++ test.py
@@ -0,0 +1,28 @@
+#importing the database
+import sqlite3
+
+#connecting to the db
+db = sqlite3.connect("next.db")
+#creating a cursor
+cursor = db.cursor()
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS users(
+    id INTEGER PRIMARY KEY NOT NULL AUTO,
+    tgID INTEGER NOT NULL
+)""")
+
+#info = ["12.12.12", "BBB", "25 свободных", "3.500", "Секрет"]
+
+#admin = ["Boris", 523492225]
+
+#cursor.execute("DROP TABLE users")
+
+#cursor.execute("""INSERT INTO admin VALUES (?, ?)""", admin)
+
+#cursor.execute("""DELETE FROM next WHERE location='BBB'""")
+
+#cursor.execute("""SELECT * FROM users""")
+#data = cursor.fetchall()
+#print(data)
+
+db.commit()
